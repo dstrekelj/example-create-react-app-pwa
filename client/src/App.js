70a9d66cbcf9f02dcbb996a990ccda5d3db80630
Domagoj Štrekelj <domagoj.strekelj@gmail.com>
2022-02-12 15:22:51 +0100

    Add note form state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import notes from "./mocks/notes.json";
 import { NoteForm } from "./components/NoteForm";
 import { NoteList } from "./components/NoteList";
+import { useCallback, useState } from "react";
 
 export default function App() {
+  const [note, setNote] = useState(null);
+
+  const handleEdit = useCallback(
+    (note) => () => {
+      console.log("Edit", note);
+      setNote(note);
+    },
+    [],
+  );
+
+  const handleDelete = useCallback(
+    (note) => () => {
+      console.log("Delete", note);
+    },
+    [],
+  );
+
+  const handleSubmit = useCallback((data) => {
+    console.log("Submit", data);
+    setNote(null);
+  }, []);
+
+  const handleReset = useCallback(() => {
+    console.log("Reset");
+    setNote(null);
+  }, []);
+
   return (
     <div>
-      <NoteList notes={notes.data} onEdit={() => {}} onDelete={() => {}} />
-      <NoteForm onSubmit={() => {}} onReset={() => {}} />
+      <NoteList notes={notes.data} onEdit={handleEdit} onDelete={handleDelete} />
+      <NoteForm note={note} onSubmit={handleSubmit} onReset={handleReset} />
     </div>
   );
 }
--- client/src/components/NoteForm.jsx
+++ client/src/components/NoteForm.jsx
@@ -1,7 +1,50 @@
+import { useCallback, useEffect, useState } from "react";
+
+const initialState = {
+  text: "",
+};
+
 export const NoteForm = (props) => {
+  const [state, setState] = useState((props.note && props.note) || initialState);
+
+  useEffect(() => {
+    setState((props.note && props.note) || initialState);
+  }, [setState, props.note]);
+
+  const handleChange = useCallback((event) => {
+    event.preventDefault();
+
+    setState((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }));
+  }, []);
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      props.onSubmit({ ...state });
+
+      setState(initialState);
+    },
+    [props, state, setState],
+  );
+
+  const handleReset = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      props.onReset();
+
+      setState(initialState);
+    },
+    [props, setState],
+  );
+
   return (
-    <form onSubmit={props.onSubmit} onReset={props.onReset}>
-      <input type="text" name="text" required />
+    <form onSubmit={handleSubmit} onReset={handleReset}>
+      <input type="text" name="text" onChange={handleChange} value={state.text} required />
       <button type="submit">Submit</button>
       <button type="reset">Clear</button>
     </form>

33116ee8bae7fd5dc69a21391b2cad33f986c495
Domagoj Štrekelj <domagoj.strekelj@gmail.com>
2022-04-09 11:04:17 +0200

    Add service worker and installation support

--- proxy/index.js
+++ proxy/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.use(
   "/api",
@@ -11,8 +14,14 @@ app.use(
     pathRewrite: { "^/api": "/" },
   }),
 );
-app.use(
-  "/",
-  createProxyMiddleware({ target: "http://localhost:3000", changeOrigin: true }),
-);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+} else {
+  app.use(
+    "/",
+    createProxyMiddleware({ target: "http://localhost:3000", changeOrigin: true }),
+  );
+}
+
 app.listen(3001);
